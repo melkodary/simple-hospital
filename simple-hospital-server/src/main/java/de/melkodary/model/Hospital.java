4122eaee3f90b55bb2642986636b0ea7ffa17650
Mahmoud ElKodary <melkodary@outlook.com>
2023-11-05 18:56:50 +0400

    Structure project for grpc

---
+++ simple-hospital-server/src/main/java/de/melkodary/model/Hospital.java
@@ -0,0 +1,37 @@
+package de.melkodary.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hospital {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "address", unique = true, nullable = false)
+    private String address;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(
+            name = "hospital_patient",
+            joinColumns = @JoinColumn(name = "hospital_id"),
+            inverseJoinColumns = @JoinColumn(name = "patient_id")
+    )
+    private Set<Patient> patients = new HashSet<>();
+}
---
+++ simple-hospital-server/src/main/java/de/melkodary/model/Patient.java
@@ -0,0 +1,35 @@
+package de.melkodary.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+
+    public enum SEX {
+        MALE,
+        FEMAlE,
+        OTHER
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    private String birthdate;
+
+    @Enumerated(EnumType.STRING)
+    private Patient.SEX sex;
+
+    @ManyToMany(mappedBy = "patients")
+    private Set<Hospital> hospitals;
+}
